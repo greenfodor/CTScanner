2fc3a459a165260d34bc1b39f513314d70e711d6
George Fodor <george.fodor@tapptitude.com>
2021-03-30 17:51:10 +0300

    add some animations

--- app/src/main/java/com/casatamplarului/ctscanner/SecondFragment.kt
+++ app/src/main/java/com/casatamplarului/ctscanner/SecondFragment.kt
@@ -13,10 +13,7 @@ import androidx.navigation.fragment.findNavController
  */
 class SecondFragment : Fragment() {
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false)
     }
